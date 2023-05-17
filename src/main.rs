47f5c7f3888664cdaa6fca41af8749dd7abb5cc4
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-05-16 22:18:36 -0700

    Get basic player movement and wall spawning

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,166 @@
 use bevy::prelude::*;
 
+// Defines the amount of time that should elapse between each physics step.
+const TIME_STEP: f32 = 1.0 / 60.0;
+
+const PLAYER_SIZE: Vec3 = Vec3::new(100.0, 25.0, 0.0);
+const GAP_BETWEEN_PLAYER_AND_FLOOR: f32 = 60.0;
+const PLAYER_SPEED: f32 = 100.0;
+// How close a player can get to a wall
+const PLAYER_PADDING: f32 = 10.0;
+
+const WALL_THICKNESS: f32 = 10.0;
+// x coordinates
+const LEFT_WALL: f32 = -450.;
+const RIGHT_WALL: f32 = 450.;
+// y coordinates
+const BOTTOM_WALL: f32 = -300.;
+const TOP_WALL: f32 = 300.;
+
+const BACKGROUND_COLOR: Color = Color::BLACK;
+const PLAYER_COLOR: Color = Color::GREEN;
+const WALL_COLOR: Color = Color::GREEN;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(ExamplePlugin)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_startup_system(setup)
+        .add_system(move_player)
+        .add_system(bevy::window::close_on_esc)
         .run();
 }
 
 #[derive(Component)]
-struct Person;
+struct Player;
 
 #[derive(Component)]
-struct Age(u8);
+struct Velocity;
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Age(18)));
-    commands.spawn((Person, Age(32)));
-    commands.spawn((Person, Age(7)));
+#[derive(Component)]
+struct Collider;
+
+enum WallLocation {
+    Left,
+    Right,
+    Bottom,
+    Top,
 }
 
-#[derive(Resource)]
-struct SayAgeTimer(Timer);
+impl WallLocation {
+    fn position(&self) -> Vec2 {
+        match self {
+            WallLocation::Left => Vec2::new(LEFT_WALL, 0.),
+            WallLocation::Right => Vec2::new(RIGHT_WALL, 0.),
+            WallLocation::Bottom => Vec2::new(0., BOTTOM_WALL),
+            WallLocation::Top => Vec2::new(0., TOP_WALL),
+        }
+    }
 
-fn say_age(time: Res<Time>, mut timer: ResMut<SayAgeTimer>, query: Query<&Age, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for age in &query {
-            println!("My age is {}!", age.0)
+    fn size(&self) -> Vec2 {
+        let arena_height = TOP_WALL - BOTTOM_WALL;
+        let arena_width = RIGHT_WALL - LEFT_WALL;
+        // Make sure we haven't messed up our constants
+        assert!(arena_height > 0.0);
+        assert!(arena_width > 0.0);
+
+        match self {
+            WallLocation::Left | WallLocation::Right => {
+                Vec2::new(WALL_THICKNESS, arena_height + WALL_THICKNESS)
+            }
+            WallLocation::Bottom | WallLocation::Top => {
+                Vec2::new(arena_width + WALL_THICKNESS, WALL_THICKNESS)
+            }
         }
     }
 }
 
-pub struct ExamplePlugin;
+// This bundle is a collection of the components that define a "wall" in our game
+#[derive(Bundle)]
+struct WallBundle {
+    // You can nest bundles inside of other bundles like this
+    // Allowing you to compose their functionality
+    sprite_bundle: SpriteBundle,
+    collider: Collider,
+}
 
-impl Plugin for ExamplePlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(SayAgeTimer(Timer::from_seconds(5.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(say_age);
+impl WallBundle {
+    // This "builder method" allows us to reuse logic across our wall entities,
+    // making our code easier to read and less prone to bugs when we change the logic
+    fn new(location: WallLocation) -> WallBundle {
+        WallBundle {
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    // We need to convert our Vec2 into a Vec3, by giving it a z-coordinate
+                    // This is used to determine the order of our sprites
+                    translation: location.position().extend(0.0),
+                    // The z-scale of 2D objects must always be 1.0,
+                    // or their ordering will be affected in surprising ways.
+                    // See https://github.com/bevyengine/bevy/issues/4149
+                    scale: location.size().extend(1.0),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: WALL_COLOR,
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+        }
     }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    let player_y = BOTTOM_WALL + GAP_BETWEEN_PLAYER_AND_FLOOR;
+
+    // Spawn the player
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, player_y, 0.0),
+                scale: PLAYER_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: PLAYER_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Player,
+    ));
+
+    // Walls
+    commands.spawn(WallBundle::new(WallLocation::Left));
+    commands.spawn(WallBundle::new(WallLocation::Right));
+    commands.spawn(WallBundle::new(WallLocation::Bottom));
+    commands.spawn(WallBundle::new(WallLocation::Top));
+}
+
+fn move_player(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
+    let mut player_transform = query.single_mut();
+    let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::A) {
+        direction -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::D) {
+        direction += 1.0;
+    }
+
+    let new_player_position = player_transform.translation.x + direction * PLAYER_SPEED * TIME_STEP;
+
+    // Update the player position,
+    // making sure it doesn't cause the player to leave the arena
+    let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PLAYER_SIZE.x / 2.0 + PLAYER_PADDING;
+    let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PLAYER_SIZE.x / 2.0 - PLAYER_PADDING;
+
+    player_transform.translation.x = new_player_position.clamp(left_bound, right_bound);
 }
